69651654805b046334bd92fe7d6037344a5d7873
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-14 02:15:53 +0700

    create path params and query api

--- app/main.py
+++ app/main.py
@@ -6,6 +6,21 @@ app = FastAPI()
 # Import local module
 from .helper import api_builder
 
+# Index
 @app.get("/", status_code=200)
 def status(response: Response):
     return api_builder.builder("Hello World! hzlnqodrey Sentiment API Work", response.status_code)
+
+# Predict
+
+# Example Path params & Query
+@app.get("/items/{item_id}", status_code=200)
+def read_item(response: Response, item_id: int, q: Union[str, None] = None):
+    result = []
+
+    result.append({
+        "item_id": item_id,
+        "q": q
+    })
+
+    return api_builder.builder(result, response.status_code)
