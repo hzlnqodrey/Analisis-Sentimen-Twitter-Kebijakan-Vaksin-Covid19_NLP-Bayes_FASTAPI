6c83740d61d47a1b1955b1f5caa483dedcc62668
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-14 02:18:57 +0700

    feat: create predict route and controller

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ def status(response: Response):
     return api_builder.builder("Hello World! hzlnqodrey Sentiment API Work", response.status_code)
 
 # Predict
+@app.get("/predict", status_code=200)
+def predict(response: Response):
+    result = ["Tahu", "ini", 2]
+    return api_builder.builder(result, response.status_code)
 
 # Example Path params & Query
 @app.get("/items/{item_id}", status_code=200)
