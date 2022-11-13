a2f744f74803c0bf5df6df1f7a9dbd1e32db3fb1
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-14 02:01:39 +0700

    create api.builder responses

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+@app.get("/", status_code=200)
+async def status(response: Response):
     return {"Hello" : "World"}
