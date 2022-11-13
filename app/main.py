3e385d03195cfa3d2bfa26ab38dc23f2af93b778
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-14 02:08:55 +0700

    api builder working perfectly!

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Union
-
 from fastapi import FastAPI, Response, Request
 
 app = FastAPI()
 
+# Import local module
+from .helper import api_builder
+
 @app.get("/", status_code=200)
-async def status(response: Response):
-    return {"Hello" : "World"}
+def status(response: Response):
+    return api_builder.builder("Hello World! hzlnqodrey Sentiment API Work", response.status_code)
