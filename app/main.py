1e14e0b5d044addc8158cc5e0e8ccd1b2990eedb
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-24 23:55:56 +0700

    well idk wtf just happened, but it works i guess

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from typing import Union
 from fastapi import FastAPI, Response, Request
 from pydantic import BaseModel
+import pandas as pd
+import os
 
 # Import local module
 from .helper import api_builder
@@ -16,7 +18,15 @@ def status(response: Response):
 # Predict
 @app.get("/predict", status_code=200)
 def predict(response: Response):
-    result = ["Tahu", "ini", 2]
+    data = pd.read_csv('hasil_klasifikasi_naive_bayes_data_tweet_8k.csv')
+
+    result = []
+    # text_clean = text_english = nlp = bayes = []
+    
+    for index, row in list(data.iterrows()):
+        result.append(row)
+
+
     return api_builder.builder(result, response.status_code)
 
 # --------------------------------------------------------------------------------------------------------------------
